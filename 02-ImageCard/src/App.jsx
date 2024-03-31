685bb52f873485a44da16270800dec1709a72fe6
CodeforNitin <codeforni3@gmail.com>
2024-04-01 01:36:20 +0530

    Props demo

--- 02-ImageCard/src/App.jsx
+++ 02-ImageCard/src/App.jsx
@@ -1,11 +1,13 @@
 import Card from "./Card.jsx"
-
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.jpg";
 
 function App() {
   return (
     <>
-      <Card/>
-      <Card/>
+      <Card img = {img1} name = "Light Yagami" age = {23} isStudent = {true}/>
+      <Card img = {img2} name = "L Lawliet"    age = {25} isStudent = {false}/>
+  
     </>
   );
 }
--- 02-ImageCard/src/Card.jsx
+++ 02-ImageCard/src/Card.jsx
@@ -1,18 +1,32 @@
-import profilePic from './assets/img1.jpeg';
-import './App.css'
+
+import "./App.css";
+
+//This function returns a single card
+
+// function Card(){
+//     return(
+//         <div className="container">
+//             <img src= {profilePic} alt="Light Yagami pic"></img>
+//             <h2>Light Yagami</h2>
+//             <p>People know me as Kira</p>
+//         </div>
+
+//     );
+
+//Using Props
 
 
-function Card(){
-    return(
-        <div className="container">
-            <img src= {profilePic} alt="Light Yagami pic"></img>
-            <h2>Light Yagami</h2>
-            <p>People know me as Kira</p>
-        </div>
-    
-    );
+function Card(props) {
 
-    
+  return (
+    <div className="container">
+      <img src={props.img}></img>
+      <h2>Name : {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+    </div>
+  );
 }
+
 
 export default Card;
