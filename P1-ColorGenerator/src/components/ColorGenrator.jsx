a97e10b62dcf354c5c23f3ca5ea353124b92b083
CodeforNitin <codeforni3@gmail.com>
2024-04-09 16:49:36 +0530

    added logic for color geenrator

--- P1-ColorGenerator/src/components/ColorGenrator.jsx
+++ P1-ColorGenerator/src/components/ColorGenrator.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
 function ColorGenrator() {
 
+    const [color, setColor] = useState('green')
+    const [typeOfColor, setTypeOfColor] = useState('HEX')
+
     const handleRGB  = ()=> {
 
+        let r = Math.floor(Math.random()*256);
+        let g = Math.floor(Math.random()*256);
+        let b = Math.floor(Math.random()*256);
+
+        setColor(`rgb(${r}, ${g}, ${b})`)
+
+        setTypeOfColor('RGB');
     }
 
     const handleHEX  = ()=> {
+        let str = "#";
 
+        const arr = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+        for (let i=0; i<6; i++){
+            const ran = Math.floor(Math.random()*arr.length);
+            str += arr[ran];
+        }
+
+        setColor(str);
+        setTypeOfColor('HEX');
     }
 
 
   return (
     <div className="container">
-      <div className="card">
-        <h2>Color</h2>
+      <div className="card" style={{backgroundColor:color}}>
+        <h2>Color Type : {typeOfColor}</h2>
+        <h2>{color}</h2>
       </div>
-      <div>
-          <button onClick={handleHEX}>Genrate HEX Color</button>
-          <button onClick={handleRGB}>Genrate RGB Color</button>
+
+        <div>
+        <button onClick={handleHEX}>Genrate HEX Color</button>
+        <button onClick={handleRGB}>Genrate RGB Color</button>
         </div>
+
     </div>
   );
 }
