8633860bd206e7f19d5f490bafcdc560c124669f
CodeforNitin <codeforni3@gmail.com>
2024-05-11 23:43:33 +0530

    setting up toggle button usiing context

--- P3-ContextAPI-ThemeChanger/src/context/ThemeContext.jsx
+++ P3-ContextAPI-ThemeChanger/src/context/ThemeContext.jsx
@@ -1,16 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
+    const [isLightTheme, setIsLightTheme] = useState(true);
+
     const theme = {
-        isLightTheme: true,
+        isLightTheme: isLightTheme,
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
     };
 
+    //toggle function
+    const toggleTheme = () => {
+        setIsLightTheme(!isLightTheme)
+    }
     return (
-        <ThemeContext.Provider value={{ ...theme }}>
+        <ThemeContext.Provider value={{ ...theme, toggleTheme}}>
             {props.children}
         </ThemeContext.Provider>
     );
