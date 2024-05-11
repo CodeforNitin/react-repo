d499dab8fc75fcfcc71a4a5a0af74f6acda9b84e
CodeforNitin <codeforni3@gmail.com>
2024-05-11 23:28:15 +0530

    accessing and setting theme using Context

--- P3-ContextAPI-ThemeChanger/src/components/Navbar.jsx
+++ P3-ContextAPI-ThemeChanger/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from './../context/ThemeContext';
 
 function Navbar() {
+
+const themeContext  = useContext(ThemeContext);
+
+const {isLightTheme, light, dark} = themeContext
+
+const theme = isLightTheme ? light : dark
+
   return (
-    <nav>
+    <nav style={{background: theme.ui, color: theme.syntax}}>
         <h1>Context App</h1>
         <ul>
             <li>Home</li>
