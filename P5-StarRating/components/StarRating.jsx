c0d29c6913e1af9b9345acb4a92287846bf3f8fe
CodeforNitin <codeforni3@gmail.com>
2024-07-09 18:03:45 +0530

    updated latest

--- P5-StarRating/components/StarRating.jsx
+++ P5-StarRating/components/StarRating.jsx
@@ -25,4 +25,25 @@ function StarRating({starCount}) {
     )
 }
 
+// className={((hoverValue==0 && index<starValue)||index<hoverValue) ? "gold":""}
+// --the intution behind this is:
+// --we want to apply gold class if (index<hovervalue)
+// --but suppose
+
+// --you selected star no 2
+// --index=1
+// --starvalue=2
+// --hovervaluu=2
+// --class gold is  applied to  star no 1 and 2
+
+// --but when you lower and hover to star no 1
+// --then index=0
+// --hovervalue=1
+// --but starvalue=2
+
+// --hence it will show 2 stars (star1 and star 2 as coloured [but you want now only star one to be coloured as])
+// --but this is not happening since hovervalue has interuupting
+
+// --hence check if hovervalue==0
+
 export default StarRating
