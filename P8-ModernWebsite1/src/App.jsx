f2deed039d6dee95f7468bf3e70d1b54e78e9247
CodeforNitin <codeforni3@gmail.com>
2024-07-15 23:12:56 +0530

    footer added

--- P8-ModernWebsite1/src/App.jsx
+++ P8-ModernWebsite1/src/App.jsx
@@ -1,12 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Footer />
       </div>
 
       <ButtonGradient />
