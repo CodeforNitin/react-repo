32a1fc63e26189e562d447a7970fbf69e5422426
CodeforNitin <codeforni3@gmail.com>
2024-07-18 03:24:18 +0530

    Done with Services section

--- P8-ModernWebsite1/src/App.jsx
+++ P8-ModernWebsite1/src/App.jsx
@@ -3,7 +3,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
-
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -11,7 +12,9 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Services />
         <Pricing />
+        <Roadmap />
         <Footer />
       </div>
 
