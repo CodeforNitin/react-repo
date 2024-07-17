029d158d507861d051f8d4db3ff470fae6b42dd0
CodeforNitin <codeforni3@gmail.com>
2024-07-17 23:05:17 +0530

    working on Mouse Parallax effect

--- P8-ModernWebsite1/src/App.jsx
+++ P8-ModernWebsite1/src/App.jsx
@@ -1,12 +1,17 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
+        <Pricing />
         <Footer />
       </div>
 
